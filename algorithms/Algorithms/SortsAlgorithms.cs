ec8d80447f8e2254f822bfdbfa9c5d396b5a1a6f
Slava <sh-e-95@mail.ru>
2022-01-04 15:53:27 +0500

    selection sort

--- algorithms/Algorithms/SortsAlgorithms.cs
+++ algorithms/Algorithms/SortsAlgorithms.cs
@@ -2,37 +2,7 @@
 
 public static class SortAlgorithms
 {
-    public static int[] BublleSort(int[] arr)
-    {
-        for (int i = 0; i < arr.Length; i++)
-        {
-            for (int j = i+1; j < arr.Length; j++)
-            {
-                if (arr[i] > arr[j])
-                {
-                    (arr[j], arr[i]) = (arr[i], arr[j]);
-                }
-            }
-        }
-
-        return arr;
-    }
-
-    public static int[] InsertSort(int[] arr)
-    {
-        for (int i = 1; i < arr.Length; i++)
-        {
-            var k = i - 1;
-            var el = arr[i];
-            while (k>=0 && arr[k]>el)
-            {
-                arr[k+1] = arr[k];
-                k--;
-            }
-            arr[k+1] = el;
-        }
-        return arr;
-    }
+    
 
     public static int[] SelectionSort(int[] arr)
     {
