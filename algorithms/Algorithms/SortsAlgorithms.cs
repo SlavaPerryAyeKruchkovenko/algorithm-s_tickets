a46b051eb39bf605bf033a819a547722dfd168d1
Slava <sh-e-95@mail.ru>
2022-01-04 17:28:51 +0500

    add shellSort

--- algorithms/Algorithms/SortsAlgorithms.cs
+++ algorithms/Algorithms/SortsAlgorithms.cs
@@ -72,10 +72,28 @@ public static class SortAlgorithms
                     needSort = true;
                 }
             }
-
             if (!needSort)
             {
                 return arr;
+            }
+        }
+        return arr;
+    }
+
+    public static int[] ShellSort(int[] arr)
+    {
+        var steep = arr.Length / 2;
+        while (steep >= 1)
+        {
+            steep = steep / 2;
+            for (var i = steep; i < arr.Length; i++)
+            {
+                var j = i;
+                while ((j >= steep) && (arr[j - steep] > arr[j]))
+                {
+                    (arr[j], arr[j-steep]) = (arr[j-steep], arr[j]);
+                    j = j - steep;
+                }
             }
         }
         return arr;
