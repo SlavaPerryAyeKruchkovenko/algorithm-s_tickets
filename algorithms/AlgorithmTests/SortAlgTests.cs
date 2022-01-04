84ccdabf5f799c20cbc4fe847bbb8ba33ea3c3f4
Slava <sh-e-95@mail.ru>
2022-01-04 17:32:20 +0500

    ShellSort

--- algorithms/AlgorithmTests/SortAlgTests.cs
+++ algorithms/AlgorithmTests/SortAlgTests.cs
@@ -10,38 +10,6 @@ public class SortAlgTests
 {
     [Theory]
     [MemberData(nameof(GetArrays))]
-    public void BubbleTest(int[] arr)
-    {
-        arr = SortAlgorithms.BubbleSort(arr);
-        var res = arr.OrderBy(x=>x).ToArray();
-        Assert.Equal(arr,res);
-    }
-    [Theory]
-    [MemberData(nameof(GetArrays))]
-    public void InsertTest(int[] arr)
-    {
-        arr = SortAlgorithms.InsertSort(arr);
-        var res = arr.OrderBy(x=>x).ToArray();
-        Assert.Equal(arr,res);
-    }
-    [Theory]
-    [MemberData(nameof(GetArrays))]
-    public void SelectionTest(int[] arr)
-    {
-        arr = SortAlgorithms.SelectionSort(arr);
-        var res = arr.OrderBy(x=>x).ToArray();
-        Assert.Equal(arr,res);
-    }
-    [Theory]
-    [MemberData(nameof(GetArrays))]
-    public void ShakerTest(int[] arr)
-    {
-        arr = SortAlgorithms.ShakerSort(arr);
-        var res = arr.OrderBy(x=>x).ToArray();
-        Assert.Equal(arr,res);
-    }
-    [Theory]
-    [MemberData(nameof(GetArrays))]
     public void ShellTest(int[] arr)
     {
         arr = SortAlgorithms.ShellSort(arr);
