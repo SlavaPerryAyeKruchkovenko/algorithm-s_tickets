95edde1a6e931c5c3798b71ee61c92ac3539b209
Slava <sh-e-95@mail.ru>
2022-01-04 15:52:49 +0500

    add SelectionSort

--- algorithms/Algorithms/SortsAlgorithms.cs
+++ algorithms/Algorithms/SortsAlgorithms.cs
@@ -33,4 +33,21 @@ public static class SortAlgorithms
         }
         return arr;
     }
+
+    public static int[] SelectionSort(int[] arr)
+    {
+        for (int i = 0; i < arr.Length-1; i++)
+        {
+            var index = i;
+            for (int j = i+1; j < arr.Length; j++)
+            {
+                if (arr[j] < arr[index])
+                {
+                    index = j;
+                }
+            }
+            (arr[index], arr[i]) = (arr[i], arr[index]);
+        } 
+        return arr;
+    }
 }
