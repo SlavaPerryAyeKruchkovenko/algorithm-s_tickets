9338170b990eed6e9f1f974428b44ff417a5b49c
Slava <sh-e-95@mail.ru>
2022-01-04 15:36:16 +0500

    add insetSort

--- algorithms/AlgorithmTests/SortAlgTests.cs
+++ algorithms/AlgorithmTests/SortAlgTests.cs
@@ -17,6 +17,14 @@ public class SortAlgTests
         var res = arr.OrderBy(x=>x).ToArray();
         Assert.Equal(arr,res);
     }
+    [Theory]
+    [MemberData(nameof(GetArrs))]
+    public void InstertTest(int[] arr)
+    {
+        arr = SortAlgorithms.InsertSort(arr);
+        var res = arr.OrderBy(x=>x).ToArray();
+        Assert.Equal(arr,res);
+    }
     public static IEnumerable<object[]> GetArrs()
     {
         yield return new object[] { new int[]{9, 7, 6, 2, 1} };
--- algorithms/Algorithms/SortsAlgorithms.cs
+++ algorithms/Algorithms/SortsAlgorithms.cs
@@ -17,4 +17,20 @@ public static class SortAlgorithms
 
         return arr;
     }
+
+    public static int[] InsertSort(int[] arr)
+    {
+        for (int i = 1; i < arr.Length; i++)
+        {
+            var k = i - 1;
+            var el = arr[i];
+            while (k>=0 && arr[k]>el)
+            {
+                arr[k+1] = arr[k];
+                k--;
+            }
+            arr[k+1] = el;
+        }
+        return arr;
+    }
 }
