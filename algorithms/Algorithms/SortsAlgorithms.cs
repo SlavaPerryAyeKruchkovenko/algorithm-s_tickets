b602661e971a9eab8374757029c686c92885a45a
Slava <sh-e-95@mail.ru>
2022-01-04 14:59:20 +0500

    Add bublesort

---
+++ algorithms/Algorithms/SortsAlgorithms.cs
@@ -0,0 +1,20 @@
+﻿namespace Algorithms;
+
+public static class SortAlgorithms
+{
+    public static int[] BublleSort(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = i+1; j < arr.Length; j++)
+            {
+                if (arr[i] > arr[j])
+                {
+                    (arr[j], arr[i]) = (arr[i], arr[j]);
+                }
+            }
+        }
+
+        return arr;
+    }
+}
