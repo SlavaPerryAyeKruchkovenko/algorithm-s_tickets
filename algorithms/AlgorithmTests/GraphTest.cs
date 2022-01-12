90a13a9fa12936df59b7749819daba73d4827be3
Slava <sh-e-95@mail.ru>
2022-01-12 22:46:02 +0500

    Djekstra

--- algorithms/AlgorithmTests/GraphTest.cs
+++ algorithms/AlgorithmTests/GraphTest.cs
@@ -17,27 +17,7 @@ public class GraphTest
         graph.AddNode("5");
         graph.AddNode("6");
     }
-    [Fact]
-    public void PrimTest()
-    {
-        var eadge1 = graph.Connect(0,1,"1",6);
-        var eadge2 =graph.Connect(0,2,"2",1);
-        var eadge3 =graph.Connect(1,2,"3",5);
-        var eadge4 =graph.Connect(0,3,"4",5);
-        var eadge5 =graph.Connect(2,3,"5",5);
-        var eadge6 =graph.Connect(1,4,"6",3);
-        var eadge7 =graph.Connect(2,4,"7",6);
-        var eadge8 =graph.Connect(3,5,"8",2);
-        var eadge9 =graph.Connect(2, 5, "9", 4);
-        var list =  GraphAlgs.PrimAlgorithm(graph);
-        var res = new List<Edge<string>>();
-        res.Add(eadge2);
-        res.Add(eadge9);
-        res.Add(eadge8);
-        res.Add(eadge3);
-        res.Add(eadge6);
-        Assert.Equal(list,res);
-    }
+    
     [Fact]
     public void DejkstraTest()
     {
--- algorithms/Algorithms/GraphAlgs.cs
+++ algorithms/Algorithms/GraphAlgs.cs
@@ -2,71 +2,7 @@ namespace Algorithms;
 using System.Linq;
 public static class GraphAlgs
 {
-    public static List<Edge<string>> PrimAlgorithm(Graph<string> graph)
-    {
-        var list = new List<Edge<string>>();
-        var notUsedEdges = new List<Edge<string>>(graph.Edges);
-        var passedNodes = new List<MyNode<string>>();
-        var notUsedNodes = new List<MyNode<string>>(graph.Nodes);
-        var rndNode = graph[0];
-        passedNodes.Add(rndNode);
-        notUsedNodes.Remove(rndNode);
-        for (var i = 0; i < graph.Length-1; i++)
-        {
-            var minEdge = notUsedEdges.First();
-            foreach (var edge in notUsedEdges)
-            {
-                if (minEdge.Value > edge.Value && (passedNodes.Contains(edge.To) && notUsedNodes.Contains(edge.From)||
-                    passedNodes.Contains(edge.From) && notUsedNodes.Contains(edge.To)))
-                {
-                    minEdge = edge;
-                }
-            }
-            list.Add(minEdge);
-            if (passedNodes.Contains(minEdge.To))
-            {
-                passedNodes.Add(minEdge.From);
-                notUsedNodes.Remove(minEdge.From);
-            }
-            else
-            {
-                passedNodes.Add(minEdge.To);
-                notUsedNodes.Remove(minEdge.To);
-            }
-        }
-        return list;
-    }
-    public static IEnumerable<MyNode<string>> DepthSearch(Graph<string> graph)
-    {
-        var visited = new List<MyNode<string>>();
-        var stack = new Stack<MyNode<string>>();
-        stack.Push(graph[0]);
-        while (stack.Count != 0)
-        {
-            var node = stack.Pop();
-            if (visited.Contains(node)) continue;
-            visited.Add(node);
-            yield return node;
-            foreach (var incidentNode in node.IncedentNode)
-                stack.Push(incidentNode);
-        }
-    }
-
-    public static IEnumerable<MyNode<string>> BreadthSearch(Graph<string> graph)
-    {
-        var visited = new List<MyNode<string>>();
-        var queue = new Queue<MyNode<string>>();
-        queue.Enqueue(graph[0]);
-        while (queue.Count != 0)
-        {
-            var node = queue.Dequeue();
-            if (visited.Contains(node)) continue;
-            visited.Add(node);
-            yield return node;
-            foreach (var incidentNode in node.IncedentNode)
-                queue.Enqueue(incidentNode);
-        }
-    }
+    
     private class NodeInformation
     {
         public NodeInformation(MyNode<string> head)
