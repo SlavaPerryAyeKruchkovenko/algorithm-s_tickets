023b4b9c9092640ccce38c1becf70e02bf44b77a
Slava <sh-e-95@mail.ru>
2022-01-12 20:52:08 +0500

    refactoring prim

--- algorithms/Algorithms/GraphAlgs.cs
+++ algorithms/Algorithms/GraphAlgs.cs
@@ -5,52 +5,35 @@ public static class GraphAlgs
     public static List<Edge<string>> PrimAlgorithm(Graph<string> graph)
     {
         var list = new List<Edge<string>>();
-        //неиспользованные ребра
-        var notPassedEdges = new List<Edge<string>>(graph.Edges);
-        //использованные вершины
+        var notUsedEdges = new List<Edge<string>>(graph.Edges);
         var passedNodes = new List<MyNode<string>>();
-        //неиспользованные вершины
-        var notPassedNodes = new List<MyNode<string>>(graph.Nodes);
-        //выбираем случайную начальную вершину
-        var bestEdge = graph.Edges.Where(x=>x.Value==graph.Edges.Min(x=>x.Value)).First();
-        passedNodes.Add(bestEdge.From);
-        notPassedNodes.Remove(bestEdge.From);
-        while (notPassedNodes.Count > 0)
+        var notUsedNodes = new List<MyNode<string>>(graph.Nodes);
+        var rndNode = graph[0];
+        passedNodes.Add(rndNode);
+        notUsedNodes.Remove(rndNode);
+        for (var i = 0; i < graph.Length-1; i++)
         {
-            var minEdgeIndex = -1; //номер наименьшего ребра
-            //поиск наименьшего ребра
-            for (var i = 0; i < notPassedEdges.Count; i++)
+            var minEdge = notUsedEdges.First();
+            foreach (var edge in notUsedEdges)
             {
-                var edge = notPassedEdges[i];
-                if (passedNodes.IndexOf(edge.To) != -1 && notPassedNodes.IndexOf(edge.From) != -1 ||
-                    passedNodes.IndexOf(edge.From) != -1 && notPassedNodes.IndexOf(edge.To) != -1)
+                if (minEdge.Value > edge.Value && (passedNodes.Contains(edge.To) && notUsedNodes.Contains(edge.From)||
+                    passedNodes.Contains(edge.From) && notUsedNodes.Contains(edge.To)))
                 {
-                    if (minEdgeIndex != -1)
-                    {
-                        if (notPassedEdges[i].Value < notPassedEdges[minEdgeIndex].Value)
-                            minEdgeIndex = i;
-                    }
-                    else
-                        minEdgeIndex = i;
+                    minEdge = edge;
                 }
             }
-            //заносим новую вершину в список использованных и удаляем ее из списка неиспользованных
-            var minEdge = notPassedEdges[minEdgeIndex];
-            if (passedNodes.IndexOf(notPassedEdges[minEdgeIndex].To) != -1)
+            list.Add(minEdge);
+            if (passedNodes.Contains(minEdge.To))
             {
-                passedNodes.Add(notPassedEdges[minEdgeIndex].From);
-                notPassedNodes.Remove(notPassedEdges[minEdgeIndex].From);
+                passedNodes.Add(minEdge.From);
+                notUsedNodes.Remove(minEdge.From);
             }
             else
             {
-                passedNodes.Add(notPassedEdges[minEdgeIndex].To);
-                notPassedNodes.Remove(notPassedEdges[minEdgeIndex].To);
+                passedNodes.Add(minEdge.To);
+                notUsedNodes.Remove(minEdge.To);
             }
-            //заносим новое ребро в дерево и удаляем его из списка неиспользованных
-            list.Add(notPassedEdges[minEdgeIndex]);
-            notPassedEdges.RemoveAt(minEdgeIndex);
         }
-
         return list;
     }
 }
